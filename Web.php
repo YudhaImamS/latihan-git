bc52edb6d3a206c22d8357e0c2639bdcf53ba02a
YudhaImamS <imamnaxx@gmail.com>
2023-04-14 17:23:39 +0700

    menambahkan Web.php,v_header.php,v_index.php,v_footer.php,v_about.php

---
+++ Web.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Web extends CI_Controller{
+    
+    function __construct(){
+        parent::__construct();
+        $this->load->helper('url');
+    }
+    
+    public function about(){
+        
+        $data['judul'] = "Halaman About";
+        $this->load->view('v_header', $data);
+        $this->load->view('v_about', $data);
+        $this->load->view('v_footer', $data);
+    }
+
+}
